8d5eea06dd4f64f9f9d7b90ec682ae091a91e116
maryfranks <maryfranks@gmail.com>
2017-11-20 09:07:48 -0500

    display messages to all connected users

--- ws-client.js
+++ ws-client.js
@@ -15,6 +15,7 @@ ws.onmessage = function(payload) {
 
 document.forms[0].onsubmit = function () {
     var input = document.getElementById('message');
+    ws.send(input.value);
     input.value = '';
 };
 
